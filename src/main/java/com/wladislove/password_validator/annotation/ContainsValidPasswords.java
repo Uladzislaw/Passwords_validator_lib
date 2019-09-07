ade703d40ac338bf09b01f2e1ed6fec6e556cca9
Uladzislaw <vladislav.sergienya@gmail.com>
2019-09-07 15:10:08 +0300

    Add annotation for scan when need to start to validate

---
+++ src/main/java/com/wladislove/password_validator/annotation/ContainsValidPasswords.java
@@ -0,0 +1,13 @@
+package com.wladislove.password_validator.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface ContainsValidPasswords {
+}
