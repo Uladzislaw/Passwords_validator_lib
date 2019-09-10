9cd97ff4620b98e79951c19507333050b941f5cd
Uladzislaw <vladislav.sergienya@gmail.com>
2019-09-10 14:07:57 +0300

    Divide method to two - one throws exception another returns boolean value

--- src/main/java/com/wladislove/password_validator/validator/PasswordValidator.java
+++ src/main/java/com/wladislove/password_validator/validator/PasswordValidator.java
@@ -1,6 +1,9 @@
 package com.wladislove.password_validator.validator;
 
 import com.wladislove.password_validator.annotation.PasswordMatches;
+import com.wladislove.password_validator.exception.PasswordsDontMatchException;
+
+import java.lang.reflect.Field;
 
 import static java.util.Objects.requireNonNull;
 
@@ -8,15 +11,38 @@ public class PasswordValidator {
 
     private static PasswordValidator validator = new PasswordValidator();
 
-    public static void validateObjectsPasswords(final Object object) {
-        requireNonNull(object);
-        validator.validatePassMatching(object.getClass());
-    }
 
-    private void validatePassMatching(final Class<?> obj) {
-        if (obj.isAnnotationPresent(PasswordMatches.class)) {
-            PasswordMatches passwordMatches = obj.getAnnotation(PasswordMatches.class);
-
+    public static void validateObjectsPasswordsOrElseThrow(final Object object) {
+        if (!validator.validatePassMatching(requireNonNull(object).getClass(), object)) {
+            throw new PasswordsDontMatchException("Passwords don't match.");
         }
     }
+
+    public static boolean validateObjectsPasswords(final Object object) {
+        return validator.validatePassMatching(requireNonNull(object).getClass(), object);
+    }
+
+    private boolean validatePassMatching(final Class<?> cl, final Object o) {
+        if (cl.isAnnotationPresent(PasswordMatches.class)) {
+            PasswordMatches passwordMatches = cl.getAnnotation(PasswordMatches.class);
+            String password = accessFieldAndGet(passwordMatches.password(), cl, o);
+            String matchingPassword = accessFieldAndGet(passwordMatches.matchingPassword(), cl, o);
+            return password.equals(matchingPassword);
+        }
+        throw new IllegalArgumentException("Annotations wasn't found.");
+    }
+
+    private String accessFieldAndGet(final String fieldName,
+                                     final Class<?> cl,
+                                     final Object o) {
+        try {
+            Field passwordField = cl.getDeclaredField(fieldName);
+            passwordField.setAccessible(true);
+            return (String) passwordField.get(o);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(
+                    "Field with " + fieldName + " wasn't found.");
+        }
+    }
+
 }
