fb222bc52d7ae762f18515faf2de79ba0224dd8d
Uladzislaw <vladislav.sergienya@gmail.com>
2019-09-12 16:25:21 +0300

    Add wrapper for password contains chars needed

---
+++ src/main/java/com/wladislove/password_validator/validator/calculator/model/PasswordWrapper.java
@@ -0,0 +1,31 @@
+package com.wladislove.password_validator.validator.calculator.model;
+
+public class PasswordWrapper {
+    private String password;
+    private int minLen;
+    private int maxLen;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+
+    public int getMinLen() {
+        return minLen;
+    }
+
+    public void setMinLen(final int minLen) {
+        this.minLen = minLen;
+    }
+
+    public int getMaxLen() {
+        return maxLen;
+    }
+
+    public void setMaxLen(final int maxLen) {
+        this.maxLen = maxLen;
+    }
+}
