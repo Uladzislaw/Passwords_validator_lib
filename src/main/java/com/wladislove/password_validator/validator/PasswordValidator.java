71254bda093cd282a4aebff074395ca9d309737e
Uladzislaw <vladislav.sergienya@gmail.com>
2019-09-12 15:55:41 +0300

    Add method to check password strength

--- src/main/java/com/wladislove/password_validator/validator/PasswordValidator.java
+++ src/main/java/com/wladislove/password_validator/validator/PasswordValidator.java
@@ -1,6 +1,7 @@
 package com.wladislove.password_validator.validator;
 
 import com.wladislove.password_validator.exception.PasswordsDontMatchException;
+import com.wladislove.password_validator.exception.WeakPasswordException;
 
 import static java.util.Objects.requireNonNull;
 
@@ -8,12 +9,18 @@ public class PasswordValidator {
 
     private static PasswordMatcherValidator passwordMatcherValidator
             = new PasswordMatcherValidator();
+    private static PasswordWeaknessValidator passwordWeaknessValidator
+            = new PasswordWeaknessValidator();
 
 
     public static void validateObjectsPasswordsOrElseThrow(final Object object) {
         if (!passwordMatcherValidator
                 .validatePassMatching(requireNonNull(object).getClass(), object)) {
             throw new PasswordsDontMatchException("Passwords don't match.");
+        }
+        if (!passwordWeaknessValidator
+                .calcPasswordStrength(object.getClass(), object)) {
+            throw new WeakPasswordException("Password too weak.");
         }
     }
 
--- src/main/java/com/wladislove/password_validator/validator/PasswordWeaknessValidator.java
+++ src/main/java/com/wladislove/password_validator/validator/PasswordWeaknessValidator.java
@@ -1,4 +1,8 @@
 package com.wladislove.password_validator.validator;
 
 public class PasswordWeaknessValidator {
+
+    public boolean calcPasswordStrength(final Class<?> cl, final Object o) {
+        return true;
+    }
 }
