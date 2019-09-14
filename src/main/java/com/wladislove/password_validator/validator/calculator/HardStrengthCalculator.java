d92d24d8bb661579e2ef0143433755212d7d58a2
Uladzislaw <vladislav.sergienya@gmail.com>
2019-09-14 13:20:19 +0300

    Add empty level divided calculator

---
+++ src/main/java/com/wladislove/password_validator/validator/calculator/HardStrengthCalculator.java
@@ -0,0 +1,10 @@
+package com.wladislove.password_validator.validator.calculator;
+
+import com.wladislove.password_validator.validator.calculator.model.PasswordWrapper;
+
+public class HardStrengthCalculator extends PasswordStrengthCalculator{
+    @Override
+    public void calculate(PasswordWrapper password) {
+
+    }
+}
---
+++ src/main/java/com/wladislove/password_validator/validator/calculator/MediumStrengthCalculator.java
@@ -0,0 +1,10 @@
+package com.wladislove.password_validator.validator.calculator;
+
+import com.wladislove.password_validator.validator.calculator.model.PasswordWrapper;
+
+public class MediumStrengthCalculator extends PasswordStrengthCalculator {
+    @Override
+    public void calculate(final PasswordWrapper password) {
+
+    }
+}
