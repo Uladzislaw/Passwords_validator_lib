2185f62ffff71b1984ca278e4aec98e707e1fe37
Uladzislaw <vladislav.sergienya@gmail.com>
2019-09-16 21:09:37 +0300

    Refactor

--- src/main/java/com/wladislove/password_validator/analysis/PasswordAnalizator.java
+++ src/main/java/com/wladislove/password_validator/analysis/PasswordAnalizator.java
@@ -8,6 +8,47 @@ import static java.lang.Character.isLowerCase;
 import static java.lang.Character.isUpperCase;
 
 public class PasswordAnalizator {
+
+
+    public PasswordStatistics gatherStatistics(final String password) {
+        PasswordStatistics statistics = new PasswordStatistics();
+        statistics.setLettersOnly(password.chars().allMatch(Character::isLetter));
+        statistics.setNumbersOnly(password.chars().allMatch(Character::isAlphabetic));
+        statistics.setNumberOfCharacters((int) password.chars().distinct().count());
+        statistics.setConsecutiveUpperCaseLetters(calcConsecutiveUpperCaseLetters(password));
+        statistics.setConsecutiveLowerCaseLetters(calcConsecutiveLowerCaseLetters(password));
+        statistics.setConsecutiveNumbers(calcConsecutiveNumbers(password));
+        statistics.setNumberOfCharacters(password.length());
+        statistics.setUpperCaseLetters((int) password.chars().filter(Character::isUpperCase).count());
+        statistics.setLowerCaseLetters((int) password.chars().filter(Character::isLowerCase).count());
+        statistics.setNumbers((int) password.chars().filter(Character::isDigit).count());
+        statistics.setNumbers((int) password.chars().filter(PasswordAnalizator::isSymbol).count());
+        statistics.setRequirements(calcPointByRequirements(password));
+        return statistics;
+    }
+
+    private int calcConsecutiveNumbers(final String password) {
+        char[] chars = password.toCharArray();
+        int result = 0;
+        for (int i = 0; i < chars.length - 1; i++) {
+            boolean isFirst;
+            if (isDigit(chars[i])) { //if digit character - step forward to next digit
+                i++;
+                isFirst = true;
+                while (i < chars.length && isDigit(chars[i])) {
+                    if (isFirst) {  //if first - add 2 points to result because of need calc both chars
+                        result += 2;
+                        isFirst = false;
+                    } else {
+                        result++;
+                    }
+                    i++;
+                }
+            }
+        }
+        return result;
+    }
+
     private static boolean isSymbol(final int c) {
         return !(isLetter(c) || isDigit(c));
     }
@@ -44,45 +85,6 @@ public class PasswordAnalizator {
                 isFirst = true;
                 while (i < chars.length && isLowerCase(chars[i])) {
                     if (isFirst) {  //if first - add 2 points to result because of need calc both letters
-                        result += 2;
-                        isFirst = false;
-                    } else {
-                        result++;
-                    }
-                    i++;
-                }
-            }
-        }
-        return result;
-    }
-
-    public PasswordStatistics gatherStatistics(final String password) {
-        PasswordStatistics statistics = new PasswordStatistics();
-        statistics.setLettersOnly(password.chars().allMatch(Character::isLetter));
-        statistics.setNumbersOnly(password.chars().allMatch(Character::isAlphabetic));
-        statistics.setNumberOfCharacters((int)password.chars().distinct().count());
-        statistics.setConsecutiveUpperCaseLetters(calcConsecutiveUpperCaseLetters(password));
-        statistics.setConsecutiveLowerCaseLetters(calcConsecutiveLowerCaseLetters(password));
-        statistics.setConsecutiveNumbers(calcConsecutiveNumbers(password));
-        statistics.setNumberOfCharacters(password.length());
-        statistics.setUpperCaseLetters((int)password.chars().filter(Character::isUpperCase).count());
-        statistics.setLowerCaseLetters((int)password.chars().filter(Character::isLowerCase).count());
-        statistics.setNumbers((int)password.chars().filter(Character::isDigit).count());
-        statistics.setNumbers((int)password.chars().filter(PasswordAnalizator::isSymbol).count());
-        statistics.setRequirements(calcPointByRequirements(password));
-        return statistics;
-    }
-
-    private int calcConsecutiveNumbers(final String password) {
-        char[] chars = password.toCharArray();
-        int result = 0;
-        for (int i = 0; i < chars.length - 1; i++) {
-            boolean isFirst;
-            if (isDigit(chars[i])) { //if digit character - step forward to next digit
-                i++;
-                isFirst = true;
-                while (i < chars.length && isDigit(chars[i])) {
-                    if (isFirst) {  //if first - add 2 points to result because of need calc both chars
                         result += 2;
                         isFirst = false;
                     } else {
